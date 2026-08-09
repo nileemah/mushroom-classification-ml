98c39be0f297302af97be73c9e480b542fed6d01
nileemah <haldonkar.nileema@gmail.com>
2026-08-09 15:56:15 +0530

    adding logistic regression option

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
+import pickle
+
 import streamlit as st
 import pandas as pd
 from io import StringIO
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, matthews_corrcoef, roc_auc_score
+
+
+
+
+model_map = {
+    "Logistic Regression": "logistic_regression_model.pkl",
+    "Decision Tree": "decision_tree_model.pkl",
+    "kNN": "knn_model.pkl",
+    "Naive Bayes": "naive_bayes_model.pkl",
+    "Random Forest": "random_forest_model.pkl"
+}
  
 st.write("""
 # Mushroom Classification app
 """)
+
+
+X_test = None
+y_test = None
+y_predict = None
+loaded_model = None
+
 
 uploaded_file = st.file_uploader("Choose a test data file")
 if uploaded_file is not None:
@@ -14,12 +38,56 @@ if uploaded_file is not None:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     #st.write(stringio)
     string_data = stringio.read()
-    st.write(string_data)
 
-option = st.selectbox(
-    "Select Model",
-    ("Logistic Regression", "Decision Tree", "kNN", "Naive Bayes","Random Forest")
-)
+    df = pd.read_csv(StringIO(string_data))
 
-# Display the selected option
-st.write("You selected:", option)
+    X = df.drop('class', axis=1)
+    y_test = df['class']
+    X = pd.get_dummies(X, drop_first=True)
+
+    X_test = X
+
+    le = LabelEncoder()
+    y_test = le.fit_transform(y_test)
+
+
+
+if X_test is not None:
+
+    option = st.selectbox(
+        "Select Model",
+        ("Logistic Regression", "Decision Tree", "kNN", "Naive Bayes","Random Forest")
+    )
+
+    # Display the selected option
+    st.write("You selected:", option)
+
+    if option == "Logistic Regression" or option == "kNN":
+        scaler = StandardScaler()
+        X_test = scaler.transform(X_test)
+
+    filename = "./model/" + model_map.get(option)
+
+    with open(filename, "rb") as file:
+        loaded_model = pickle.load(file)
+
+    y_predict = loaded_model.predict(X_test)
+
+    st.write(f"Predictions: {y_predict}")
+
+
+st.write("## Model Evaluation Metrics")
+
+if y_predict is not None:
+   
+    ascore=accuracy_score(y_test, y_predict)
+    pscore=precision_score(y_test, y_predict)
+    rscore=recall_score(y_test, y_predict)
+    f1=f1_score(y_test, y_predict)
+
+    st.write(f"Accuracy Score: {ascore}")
+    st.write(f"Precision Score: {pscore}")
+    st.write(f"Recall Score: {rscore}")
+    st.write(f"F1 Score: {f1}")
+else:
+    st.write("Please upload a test data file and select a model to see evaluation metrics.")
